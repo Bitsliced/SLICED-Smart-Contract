a8a2bc6a0166d5d2a808104efa646cc838495f77
Bitsliced <94630393+Bitsliced@users.noreply.github.com>
2022-10-23 12:00:22 +0200

    Update Sliced.sol

--- Sliced.sol
+++ Sliced.sol
@@ -11,8 +11,8 @@ contract Sliced is ERC20Burnable,Ownable,Pausable {
 
 
 	address constant team = 0x90F79bf6EB2c4f870365E785982E1f101E93b906;
-    address public  sale = 0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65;
-    address constant marketing = 0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc;
+    	address public  sale = 0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65;
+    	address constant marketing = 0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc;
 	address constant airdrop = 0x976EA74026E726554dB657fA54763abd0C3a0aa9;
 	address constant liquidity = 0x14dC79964da2C08b23698B3D3cc7Ca32193d9955;
 	address constant reward = 0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f;
